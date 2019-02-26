a635b79891756a779fba9aac661d133d9f7060ef
Stella Kim <stellahkim93@gmail.com>
2019-02-26 17:10:54 -0500

    completely filled dataset

--- missing_imputation.py
+++ missing_imputation.py
@@ -10,7 +10,7 @@ import pandas as pd
 from sklearn.neighbors import KNeighborsClassifier, KNeighborsRegressor
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 
-# cd Documents/NYC\ Data\ Science\ Academy/HousingPrice_ML_Project/
+# cd ~/Documents/NYC\ Data\ Science\ Academy/HousingPrice_ML_Project/
 train_data = pd.read_csv("data/train.csv")
 train_data = train_data.drop("Id", axis = 1)
 
@@ -40,55 +40,55 @@ train_data.isnull().sum(axis = 1).sort_values(ascending = False)/train_data.shap
 train_data.columns
 train_data.columns[train_data.isnull().any(axis = 0)]
 ## Filling in "NaN" in Alley with "None"
-train_data['Alley'].fillna("None", inplace = True)
+train_data['Alley'].fillna("NA", inplace = True)
 
 # MasVnrType: NaN values are the same for type and area -- assume "None" and "0"
 train_data[train_data['MasVnrType'].isna()].index
 train_data[train_data['MasVnrArea'].isna()].index
-train_data['MasVnrType'].fillna("None", inplace = True)
+train_data['MasVnrType'].fillna("NA", inplace = True)
 train_data['MasVnrArea'].fillna(0, inplace = True)
 
 # Empty BsmtQual = no basement (0 sq ft)
 train_data[train_data['BsmtQual'].isna()].index
 train_data[train_data['TotalBsmtSF'] == 0].index
-train_data['BsmtQual'].fillna("None", inplace = True)
+train_data['BsmtQual'].fillna("NA", inplace = True)
 
 # Empty BsmtCond = no basement (0 sq ft)
 train_data[train_data['BsmtCond'].isna()].index
 train_data[train_data['TotalBsmtSF'] == 0].index
-train_data['BsmtCond'].fillna("None", inplace = True)
+train_data['BsmtCond'].fillna("NA", inplace = True)
 
 
 # No basement
 train_data[train_data['BsmtFinType1'].isna()].index
 train_data[train_data['TotalBsmtSF'] == 0].index
-train_data['BsmtFinType1'].fillna("None", inplace = True)
+train_data['BsmtFinType1'].fillna("NA", inplace = True)
 
 # No basement
 train_data[train_data['BsmtFinType1'].isna()].index
 train_data[train_data['TotalBsmtSF'] == 0].index
-train_data['BsmtFinType1'].fillna("None", inplace = True)
+train_data['BsmtFinType1'].fillna("NA", inplace = True)
 
 # No fireplace
-train_data['FireplaceQu'].fillna("None", inplace = True)
+train_data['FireplaceQu'].fillna("NA", inplace = True)
 train_data['Fireplaces'].value_counts()
 
 
 # No garage
 train_data['GarageArea'].value_counts()
-train_data['GarageType'].fillna("None", inplace = True)
-train_data['GarageYrBlt'].fillna("None", inplace = True)
-train_data['GarageFinish'].fillna("None", inplace = True)
-train_data['GarageQual'].fillna("None", inplace = True)
-train_data['GarageCond'].fillna("None", inplace = True)
+train_data['GarageType'].fillna("NA", inplace = True)
+train_data['GarageYrBlt'].fillna("NA", inplace = True)
+train_data['GarageFinish'].fillna("NA", inplace = True)
+train_data['GarageQual'].fillna("NA", inplace = True)
+train_data['GarageCond'].fillna("NA", inplace = True)
 
 # No pool
 sum(train_data['PoolQC'].isna())
 train_data['PoolArea'].value_counts()
-train_data['PoolQC'].fillna("None", inplace = True)
+train_data['PoolQC'].fillna("NA", inplace = True)
 
 # No fence
-train_data['Fence'].fillna("None", inplace = True)
+train_data['Fence'].fillna("NA", inplace = True)
 
 
 # Fill in MiscFeature
@@ -98,7 +98,7 @@ pd.Index.difference(train_data[train_data['MiscVal'] == 0].index,
         train_data[train_data['MiscFeature'].isna()].index)
 train_data.iloc[873][['MiscVal', 'MiscFeature']]
 train_data.iloc[1200][['MiscVal', 'MiscFeature']]
-train_data['MiscFeature'].fillna("None", inplace = True)
+train_data['MiscFeature'].fillna("NA", inplace = True)
 
 
 # We decided to fill in the onemissing "Electrical" value with "SBrkr" since this is the most common one
@@ -106,32 +106,32 @@ train_data['Electrical'].value_counts()
 train_data['Electrical'].fillna("SBrkr", inplace = True)
 
 # Empty BsmtQual = no basement (0 sq ft)
-# For the indices where there is no basement, we filled the missing BsmtQual with "None"
+# Index 948 "has a basement" but we will just fill this with "None" for simplicity
 train_data['BsmtExposure'].value_counts()
-train_data[train_data['BsmtExposure'].isna()].index
-train_data[train_data['TotalBsmtSF'] == 0].index
-pd.Index.difference(train_data[train_data['BsmtExposure'].isna()].index,
-                              train_data[train_data['TotalBsmtSF'] == 0].index)
-
-indices = train_data[train_data['BsmtExposure'].isna()].index.drop(948).tolist()
-
-for idx in indices:
-    train_data.loc[idx, 'BsmtExposure'] = "None"
-
+train_data['BsmtExposure'].fillna("NA", inplace = True)
 
 # For indices wtih no basement, we fill in BsmtFinType2 with "None"
+# Index 332 "has a basement" but we will just fill this with "None" for simplicity
 train_data['BsmtFinType2'].value_counts()
-train_data[train_data['BsmtFinType2'].isna()].index
-train_data[train_data['TotalBsmtSF'] == 0].index
-pd.Index.difference(train_data[train_data['BsmtFinType2'].isna()].index,
-                               train_data[train_data['TotalBsmtSF'] == 0].index)
+train_data['BsmtFinType2'].fillna("NA", inplace = True)
 
-indices = train_data[train_data['BsmtFinType2'].isna()].index.drop(332).tolist()
 
-for idx in indices:
-    train_data.loc[idx, 'BsmtFinType2'] = "None"
 
-del (idx, indices)
+#################### Filling in LotFrontage with log(LotArea) #################
+#We can impute the missing data with the log-transform of lot area, correlation ~0.65
+train_data[['LotFrontage','LotArea']].corr()
+pd.concat([train_data['LotFrontage'], np.log(train_data['LotArea'])], axis = 1).corr()
+
+train_data['LotFrontage'] = np.log(train_data['LotArea'])
+
+
+#import seaborn as sns
+#import matplotlib.pyplot as plt
+
+#np.std(train_data['LotFrontage'])
+
+#sns.regplot('LotFrontage', np.log(train_data['LotArea']), data = train_data)
+
 
 
 
@@ -171,73 +171,6 @@ for col in scaled_num_features.columns:
         train_data[col] = scaled_num_features[col]
 
 del (col, num_features, scaled_LF, na_LF, scaled_rest, scaled_num_features)
-
-
-
-# Next, use KNN to impute the missing LotFrontage values
-k = round(np.sqrt(sum(train_data['LotFrontage'].notna())))
-knn_r = KNeighborsRegressor(n_neighbors = k)
-
-x_columns = ['LotArea']
-y_columns = ['LotFrontage']
-
-X = train_data[train_data['LotFrontage'].notna()]['LotArea'].values.reshape(1, -1)
-y = train_data[train_data['LotFrontage'].notna()]['LotFrontage'].values.reshape(1,-1)
-
-X = train_data[train_data['LotFrontage'].notna()][x_columns]
-y = train_data[train_data['LotFrontage'].notna()][y_columns]
-
-knn_r.fit(X, y)
-knn_r.predict(X)
-X_predict = np.array(train_data[train_data['LotFrontage'].isna()]['LotArea']).reshape(1,-1)
-knn_r.predict(X_predict)
-
-
-
-
-
-
-
-
-# For the remaining missing basement values, we will be using KNeighborsClassifier and 
-# the columns ['BsmtQual', 'BsmtCond', 'BsmtExposure', 'BsmtFinType1', 'BsmtFinSF1',
-# 'BsmtFinType2', 'BsmtFinSF2', 'BsmtUnfSF', 'TotalBsmtSF'] to impute the missing value
-k = round(np.sqrt(sum(train_data['BsmtExposure'].notna())))
-knn_c = KNeighborsClassifier(n_neighbors = k)
-
-
-## For BsmtExposure
-bsmt = train_data[['BsmtQual', 'BsmtCond', 'BsmtExposure', 'BsmtFinType1', 'BsmtFinSF1',
-                   'BsmtFinType2', 'BsmtFinSF2', 'BsmtUnfSF', 'TotalBsmtSF']]
-
-X = pd.get_dummies(bsmt[bsmt.index != 948], columns = ['BsmtQual', 'BsmtCond', 'BsmtFinType1', 'BsmtFinType2'], drop_first = True)
-X.drop('BsmtExposure', axis = 1, inplace = True)
-y = pd.get_dummies(bsmt[bsmt.index != 948]['BsmtExposure'])
-
-knn_c.fit(X,y)
-knn_c.predict(X)
-
-
-basement_target = train_data.loc[948]['BsmtExposure']
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# For the missing value in LotFrontage, we will be using KNeighborsRegressor
-# using the LotFrontage and LotArea
-
 
 
 train_data.columns
