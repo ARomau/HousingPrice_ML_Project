9f386509fca098cdd6cfe68565ffe9cda6456556
Stella Kim <stellahkim93@gmail.com>
2019-02-27 19:44:52 -0500

    cleaned, standardized to train set

--- data_standardization.py
+++ data_standardization.py
@@ -9,6 +9,7 @@ from scipy import stats
 from astropy.table import Table
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
+import pandas as pd
 
 ###############################################################################
 ########################## BOX-COX TRANSFORMATIONS ############################
@@ -28,7 +29,7 @@ def normtesttab(x):
 
 normtesttab(train_fe['GarageScore'])
 normtesttab(train_fe['TotalSF'])
-normtesttab(train_fe['SalePrice'])
+#normtesttab(train_fe['SalePrice'])
 
 #https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.boxcox.html
 xt, maxlog, interval = stats.boxcox(train_fe['GarageScore'] + abs(min(train_fe['GarageScore']))+1, alpha=0.05)
@@ -51,12 +52,40 @@ train_fe['SalePrice'] = (train_fe['SalePrice'] + abs(min(train_fe['GarageScore']
 ############################# STANDARDIZATION #################################
 ###############################################################################
 
+to_scale = train_fe.drop(columns = ['SalePrice'])
+colnames = to_scale.columns
 scaler = StandardScaler()
+scaler.fit(to_scale)
+scaled_features = pd.DataFrame(scaler.transform(to_scale),
+             columns = to_scale.columns)
+scaler2 = StandardScaler()
+scaler2.fit(train_fe[['SalePrice']])
+scaled_price = pd.DataFrame(scaler2.transform(train_fe[['SalePrice']]),
+                            columns = ['SalePrice'])
 
-scaled_features = pd.DataFrame(scaler.fit_transform(train_fe),
-             columns = train_fe.columns)
+scaled_features = pd.concat([scaled_features, scaled_price], axis = 1)
 
-#scaled_features.hist(figsize=(25, 25))
 
-import pandas as pd
-train_fe.to_csv("data/cleaned_standardized_TEST.csv", index = False)
+#scaled_features.to_csv("data/cleaned_standardized_fe.csv", index = False)
+
+
+
+
+# this is just for the test set, to get the same # of columns to transform
+
+#train.columns.tolist()
+#dir(train.columns)
+
+#train.columns.where(~train.columns.isin(train_fe.columns)).dropna()
+#housestyle = pd.DataFrame(np.zeros(len(test)), columns = ["HouseStyle_2.5Fin"])
+#train_fe = pd.concat([train_fe, housestyle], axis = 1)
+#train_fe = train_fe.reindex(columns = colnames)
+#
+#
+#scaled_features = pd.DataFrame(scaler.transform(train_fe),
+#             columns = train_fe.columns)
+#
+#
+##scaled_features.hist(figsize=(25, 25))
+#scaled_features.to_csv("data/cleaned_standardized_TEST.csv", index = False)
+
